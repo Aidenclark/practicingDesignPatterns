d935449b1fbe4da95990fe04ccfa990a6529ee59
Aiden Clark <70608824+Aidenclark@users.noreply.github.com>
2023-06-07 21:00:27 -0500

    Update factoryMethod.cs

--- C#/factoryMethod.cs
+++ C#/factoryMethod.cs
@@ -1,5 +1,28 @@
 using System;
 
+/* Let's consider a real-life example of a document converter application. This application needs to convert documents 
+from various formats (such as PDF, DOCX, and TXT) into a common format, such as plain text.
+
+To implement this functionality using the Factory Method pattern, we can define an abstract class or interface called 
+`Document` that represents the common interface for all documents. This class can have a method like `convertToPlainText()` 
+that converts the document into plain text.
+
+Next, we create concrete classes that implement the `Document` interface for each document format. For example, we can have 
+classes like `PDFDocument`, `DOCXDocument`, and `TXTDocument`. Each of these classes would override the `convertToPlainText()` 
+method to implement the specific conversion logic for that document format.
+
+Finally, we create a `DocumentFactory` class that acts as the factory responsible for creating the appropriate `Document` objects. 
+This class can have a method like `createDocument(String filePath)` that takes a file path as input and returns the corresponding 
+`Document` object based on the file's format. Inside this method, we can check the file extension or perform some other format 
+detection logic to determine the appropriate document type to instantiate.
+
+Here's an example code snippet to illustrate this:
+*/
+
+
+// In Java, an interface is a blueprint of a class that defines a set of methods that must be implemented by any class that claims 
+// to implement that interface. It establishes a contract or an agreement for classes to adhere to.
+
 interface Document
 {
     string ConvertToPlainText();
