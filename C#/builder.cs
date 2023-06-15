1ef46563836120717dcc08c334ff528a83d81b46
Aiden Clark <70608824+Aidenclark@users.noreply.github.com>
2023-06-15 15:31:21 -0500

    Update builder.cs

--- C#/builder.cs
+++ C#/builder.cs
@@ -1,4 +1,21 @@
+/*
+In this C# implementation, the classes are defined with appropriate properties, methods, and syntax based on the 
+Python code provided. The basic structure and logic of the Builder pattern remain the same. The House class has 
+properties representing different parts of the house, and the HouseBuilder and its subclasses define the building 
+process for different types of houses. The HouseDirector class orchestrates the construction process. The Main 
+method is used for testing and outputting the results.
 
+
+
+In the C# code, the get and set keywords are used to define properties in the House class. Properties provide a 
+way to encapsulate fields and allow controlled access to their values.
+
+By using properties, you can define the behavior when getting or setting the values of the fields. This allows 
+you to add additional logic, such as validation or formatting, before getting or setting the values.
+
+In the House class, the properties Foundation, Walls, Roof, Windows, and Doors are defined using the get and set 
+keywords. This allows external code to access and modify these values using the property syntax.
+*/
 
 
 
